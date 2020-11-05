4118e52d5fe5f60329a92d7976978f8c995d4e67
Abhineet Mishra <42692851+abhineetmishra64@users.noreply.github.com>
2020-11-05 19:28:44 +0530

    Basics of NumPy

---
+++ numpy2.py
@@ -0,0 +1,13 @@
+import numpy as np
+k=np.arange(2,19)
+print("Elements are:")
+print(k)
+
+k1=np.arange(2,19,3)
+print("Element now:")
+print(k1)
+
+k2=np.arange(1,2,.003)
+print("Elements are:")
+print(k2)
+print("No. of elements in k2:",len(k2))
